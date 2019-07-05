c29945b4ddb11549fab7711d7e4ef6be062f2e7c
Gary Blaser <glblaser@gmail.com>
2019-07-05 13:48:31 -0500

    Fixed boilerplate to resemble example FEC

--- client/index.jsx
+++ client/index.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Cart from "./Cart.jsx";
+import Cart from './Cart.jsx';
 
-ReactDOM.render(<Cart />, document.getElementById("Cart"));
+window.Cart = Cart;
