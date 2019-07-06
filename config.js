5d7c896694b1f25fd28a27cdb298ef287e8c288f
Gary Blaser <glblaser@gmail.com>
2019-07-06 15:12:53 -0500

    Setup PM2

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  PORT: process.env.PORT,
+  HOST: '127.0.0.1',
+  MONGO_USER: process.env.MONGO_USER,
+  MONGO_PASS: process.env.MONGO_PASS
+}
+
--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const db = require('../database/db');
+const path = require('path')
 const port = process.env.PORT || 3099;
+const host = process.env.HOST || '0.0.0.0'
 
 const app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -12,7 +14,6 @@ app.use(express.static(__dirname + '/../dist'));
 
 
 
-
 app.listen(port, function() {
-  console.log(`listening on port ${port}!`);
+  console.log(`Running on https://${host}:${port}!`);
 })
