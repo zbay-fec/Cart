c88072d355b26b5234fc7450458697d89bf972d6
Gary Blaser <glblaser@gmail.com>
2019-07-06 15:31:44 -0500

    Setup PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'Cart',
+    script: './dist/bundle.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-3-13-255-0.us-east-2.compute.amazonaws.com',
+      key: '~/.ssh/Zbay.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:zbay-fec/Cart.git',
+      path: '/home/ubuntu/Zbay',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
--- server/server.js
+++ server/server.js
@@ -15,4 +15,4 @@ app.use(express.static(__dirname + '/../dist'));
 
 app.listen(port, function() {
   console.log(`listening on port ${port}!`);
-})
+}) 
