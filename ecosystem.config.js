79d096c1512a767226d44dab4ecaa15dc19be0b8
Gary Blaser <glblaser@gmail.com>
2019-07-06 15:34:09 -0500

    Setup PM2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
       user: 'ubuntu',
       host: 'ec2-3-13-255-0.us-east-2.compute.amazonaws.com',
       key: '~/.ssh/Zbay.pem',
-      ref: 'origin/master',
+      ref: 'origin/Deployment',
       repo: 'git@github.com:zbay-fec/Cart.git',
       path: '/home/ubuntu/Zbay',
       'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
