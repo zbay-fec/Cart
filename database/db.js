2f8913a3d139fd349dd36ab3e8508653a478e34e
Gary Blaser <glblaser@gmail.com>
2019-07-09 00:17:21 -0500

    Added mongo db schema

---
+++ database/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const itemsSchema = mongoose.Schema({
+  _id: String,
+  name: String,
+  price: String,
+  image1: String,
+  image2: String,
+  image3: String,
+  image4: String,
+  condition: String,
+  category: String
+});
+
+const Item = mongoose.model('Item', itemsSchema);
+
+module.exports = { Item };
