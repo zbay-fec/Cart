6640a0945646acb0a715fec6ccb4e6bead663c54
Gary Blaser <glblaser@gmail.com>
2019-07-09 14:02:28 -0500

    Cleaned up some console.logs and refactored to use .env file

--- server/server.js
+++ server/server.js
@@ -9,11 +9,7 @@ const host = process.env.HOST || '0.0.0.0'
 // need for mongo to find env
 require('dotenv').config()
 
-// console.log('env.pw is '. process.env.MONGO_PW)
-
-// mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PW}@feccarousel-satgh.mongodb.net/test?retryWrites=true&w=majority`, { useNewUrlParser: true });
-mongoose.connect(`mongodb+srv://Gary:ZbayCart@zbaycart-tvfyb.mongodb.net/ZbayCart?retryWrites=true&w=majority`, { useNewUrlParser: true });
-
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PW}@zbaycart-tvfyb.mongodb.net/ZbayCart?retryWrites=true&w=majority`, { useNewUrlParser: true });
 
 const app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -25,20 +21,16 @@ app.get('/items', (req, res) => {
   Item.find()
     .exec()
     .then(results => {
-      console.log('get request results are: ', results)
-      console.log('pw is', process.env.MONGO_PW)
       res.status(200).send(results)
     })
     .catch(err => res.status(500).json({message: 'There was an error with your request', error: err}));
 });
 
 //format for request must be like http://127.0.0.1:3099/item:?id=RAI357e
-app.get('/item:', (req, res) => {
-  console.log('req.query is: ', req.query)
+app.get('/item:id', (req, res) => {
   Item.findById(req.query.id)
     .exec()
     .then(results => {
-      console.log('results are ', results)
       res.status(200).send(results)
     })
     .catch(err => {
